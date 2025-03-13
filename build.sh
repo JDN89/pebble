736f0aa8031360073a8d2ef9bcac9bb002e89a81
JDN89 <jan.de.niels@gmail.com>
2025-03-13 22:28:47 +0100

    update the build script

--- build.sh
+++ build.sh
@@ -12,5 +12,10 @@ gcc -Wall -Wextra -pedantic *.c -o pebble
 # Check CLI argument
 if [[ $1 == "run" ]]; then
     rm -f pebble  # Remove old executable if it exists
-    gcc -Wall -Wextra -pedantic  *.c -o pebble && ./pebble
+    gcc -Wall -Wextra -pedantic  *.c src/*. -o pebble && ./pebble
+fi
+
+if [[ $1 == "test_lexer" ]]; then
+    rm -f test_lexer  # Remove old executable if it exists
+    gcc -Wall -Wextra -pedantic  main.c src/test_lexer.c -o test_lexer && ./test_lexer
 fi
